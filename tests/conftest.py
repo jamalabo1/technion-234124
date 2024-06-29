050e8f6c3b561b2cf6ceece0464c5b4419c876c0
Jamal Abo Mokh <25992049+jamalabo1@users.noreply.github.com>
2024-06-29 14:22:26 +0300

    change test pull

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,12 +4,23 @@ from engine import utils
 from engine import common
 
 
+def pytest_addoption(parser):
+    parser.addoption(
+        "--valgrind",
+        action="store_true",
+        default=False,
+        help="run tests with valgrind"
+    )
+
+
 def pytest_generate_tests(metafunc):
     test_target_data = []
     settings = utils.load_settings()
+    valgrind = metafunc.config.getoption("valgrind")
+
     for target in settings:
         utils.change_permission(target)
-        
+
         options = settings[target]
         if "generated" in options:
             for op in options["generated"]:
@@ -17,7 +28,8 @@ def pytest_generate_tests(metafunc):
                     path_vars = op["path_vars"]
                     variables = {key: test_case.joinpath(path_vars[key]) for key in path_vars}
 
-                    commands = common.get_generated_test_commands(target, op, variables)
+                    commands = common.get_generated_test_commands(target, op, variables, valgrind)
+
                     test_target_data.append(
                         (
                             target,
