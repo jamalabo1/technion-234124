ae90a0eb31b80ec532e58adfa5692671e4ab5fd6
Jamal Abo Mokh <jamalabomokh490@gmail.com>
2024-07-04 02:03:17 +0300

    update cmake

--- cmake/core.cmake
+++
@@ -1,8 +0,0 @@
-function(install_binaries target binaries_dir)
-    add_custom_command(
-            TARGET "${target}" POST_BUILD
-            COMMAND ${CMAKE_COMMAND} -E copy_directory
-            "${binaries_dir}/bin/$<CONFIGURATION>"
-            $<TARGET_FILE_DIR:${target}>
-    )
-endfunction()
