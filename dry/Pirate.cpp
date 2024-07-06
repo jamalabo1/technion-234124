df740dbca853121d36130df04a64d01ba601d560
farahqub <farah.qub@campus.technion.ac.il>
2024-07-06 14:25:58 +0300

    3.3 dry changes

--- dry/Pirate.cpp
+++ dry/Pirate.cpp
@@ -2,7 +2,7 @@
 #include "Pirate.h"
 
 
-Pirate::Pirate(const string& name): name(name) {}
+Pirate::Pirate(const string& name, int bounty): name(name), bounty(bounty) {}
 
 
 void Pirate::setName(const string& name){
@@ -15,7 +15,16 @@ std::string Pirate::getName(){
 }
 
 
+void Pirate::setBounty(int bounty) {
+    this->bounty=bounty;
+}
+int Pirate::getBounty() {
+    return this->bounty;
+}
+
+
 std::ostream &operator<<(std::ostream &os, const Pirate &pirate){
     os << pirate.name;
+    os << pirate.bounty;
     return os;
 }
