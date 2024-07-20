150f75c332691ed01ce39b2cee1e8e6b7a2e67f2
farahqub <farah.qub@campus.technion.ac.il>
2024-07-20 21:24:18 +0300

    implemented sorted list

--- src/SortedList.h
+++ src/SortedList.h
@@ -14,8 +14,10 @@ namespace mtm {
             Holder *chain;
 
         public:
-            Holder(T data) : data(data), chain(nullptr) {};
+            Holder(T data)
+                    : data(data), chain(nullptr) {};
 
+            //This is a copy constructor for Holder
             Holder(const Holder &copy) : Holder(copy.data) {
                 if (copy.chain != nullptr) {
                     // copy the chain
@@ -23,6 +25,8 @@ namespace mtm {
                 }
             }
 
+            //check ensures that if you are assigning an object to itself,
+            //it does nothing and returns *this to avoid self-assignment issues.
             Holder &operator=(const Holder &other) {
                 if (this == &other) return *this;
 
@@ -32,14 +36,24 @@ namespace mtm {
 
         };
 
-        Holder head;
+        Holder *head;
         int length;
 
     public:
-        SortedList() : head(nullptr), length(0) {};
+        //constructor of the sortedlist initialization empty
+        //1. SortedList() - creates an empty list.
+        SortedList()
+                : head(nullptr), length(0) {};
 
-        SortedList(const SortedList<T> &copy) : head(copy.head), length(copy.length) {}
+        //constructor of the sortedlist initialization of the copy
+        //2. copy constructor
+        SortedList(const SortedList<T> &copy)
+                : head(copy.head), length(copy.length) {}
 
+
+        //declares the assignment operator for the SortedList class template.
+        // It allows one SortedList object to be assigned to another.
+        //3. operator= - assignment operator
         SortedList<T> &operator=(const SortedList<T> &other) {
             if (this == &other) return *this;
 
@@ -48,56 +62,146 @@ namespace mtm {
             length = other.length;
         }
 
+        //4. ~SortedList() - destructor
+        ~SortedList() {
+            Holder *current = head;
+            while (current != nullptr) {
+                Holder *temp = current;
+                current = current->chain;
+                // Without temp, once current is advanced,
+                // the reference to the Holder that needs to be deleted would be lost.
+                delete temp;
+            }
+
+        }
+
+        //* iterator:
+        //* 5. class ConstIterator;
         class ConstIterator;
-        /**
-         *
-         * the class should support the following public interface:
-         * if needed, use =defualt / =delete
-         *
-         * constructors and destructor:
-         * 1. SortedList() - creates an empty list.
-         * 2. copy constructor
-         * 3. operator= - assignment operator
-         * 4. ~SortedList() - destructor
-         *
-         * iterator:
-         * 5. class ConstIterator;
-         * 6. begin method
-         * 7. end method
-         *
-         * functions:
-         * 8. insert - inserts a new element to the list
-         * 9. remove - removes an element from the list
-         * 10. length - returns the number of elements in the list
-         * 11. filter - returns a new list with elements that satisfy a given condition
-         * 12. apply - returns a new list with elements that were modified by an operation
-         */
+
+        //6. begin method
+        ConstIterator begin() const {
+            return ConstIterator(head);
+        }
+
+        //7. end method
+        ConstIterator end() const {
+            return ConstIterator(nullptr);
+        }
+
+        //8. insert - inserts a new element to the list
+        void insert(T input) {
+            auto *NewInput = new Holder(input);
+            if (head == nullptr || input > head->data) {
+                NewInput->chain = head;
+                head = NewInput;
+            } else {
+                auto *current = head;
+                //find the last place that the input is bigger than the value in it
+                while (current->chain != nullptr && current->chain->data >= input) {
+                    current = current->chain;
+                }
+                //once you find it update the next and put it in the right place
+                NewInput->chain = current->chain;
+                current->chain = NewInput;
+            }
+            // we added a new number then the length will increase
+            length++;
+        }
+
+        //9. remove - removes an element from the list
+        void remove(ConstIterator toDelete) {
+            //check if empty
+            if (head == nullptr)
+                return;
+
+            Holder *current = head;
+            Holder *previous = nullptr;
+
+            while (current != nullptr && current != toDelete.holder) {
+                previous = current;
+                current = current->chain;
+            }
+
+            if (current == nullptr) return; //the line was not found
+
+            if (previous == nullptr) {
+                // in the situation where the list is just one number
+                head = current->chain;
+            } else {
+                // make the previous one make two steps forward
+                previous->chain = current->chain;
+            }
+            // manged to find the number and to delete it
+            delete current;
+            length--;
+        }
+
+        //10. length - returns the number of elements in the list
+        int size() const {
+            return length;
+        }
+
+        // 11. filter - returns a new list with elements that satisfy a given condition
+        SortedList filter(bool *prediction(T)) const {
+            SortedList<T> newList;
+            Holder *current = head;
+            while (current != nullptr) {
+                if (prediction(current->data)) {
+                    newList.insert(current->data);
+                }
+                current = current->chain;
+            }
+            return newList;
+        }
+
+        //12. apply - returns a new list with elements that were modified by an operation
+        SortedList apply(T *func(T)) const {
+            SortedList<T> newList;
+            Holder *current = head;
+            while (current != nullptr) {
+                newList.insert(func(current->data));
+                current = current->chain;
+            }
+            return newList;
+        }
 
     };
 
     template<class T>
     class SortedList<T>::ConstIterator {
-        /**
-         * the class should support the following public interface:
-         * if needed, use =defualt / =delete
-         *
-         * constructors and destructor:
-         * 1. a ctor(or ctors) your implementation needs
-         * 2. copy constructor
-         * 3. operator= - assignment operator
-         * 4. ~ConstIterator() - destructor
-         *
-         * operators:
-         * 5. operator* - returns the element the iterator points to
-         * 6. operator++ - advances the iterator to the next element
-         * 7. operator!= - returns true if the iterator points to a different element
-         *
-         */
+    private:
+        Holder *holder;
+
     public:
-        ConstIterator() = default;
+        //* constructors and destructor:
+        //* 1. a ctor(or ctors) your implementation needs
+        //* 2. copy constructor
+        //* 3. operator= - assignment operator
+        //* 4. ~ConstIterator() - destructor
+        ConstIterator(Holder *holder) : holder(holder) {}
 
-        const T &operator*();
+        ConstIterator(const ConstIterator &other) = default;
+
+        ConstIterator &operator=(const ConstIterator &other) = default;
+
+        ~ConstIterator() = default;
+
+        //5. operator* - returns the element the iterator points to
+        const T &operator*() const {
+            return holder->data;
+        }
+
+        //6. operator++ - advances the iterator to the next element
+        ConstIterator &operator++() {
+            if (holder == nullptr) return std::out_of_range("Iterator out of range");
+            holder = holder->chain;
+            return *this;
+        }
+
+        //7. operator!= - returns true if the iterator points to a different element
+        bool operator!=(const ConstIterator &other) const {
+            return holder != other.holder;
+        }
     };
-
 }
-
